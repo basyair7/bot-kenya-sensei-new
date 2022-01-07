9365d9b5ade97a5b78f0f7da3855496bc119304d
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-08 01:52:50 +0700

    Update play.ts

--- commands/play.ts
+++ commands/play.ts
@@ -123,8 +123,8 @@ exports.run = async (client, message, args) => {
         const source = await ytdl(track.url, {
           filter: "audioonly",
           bitrate: 192000,
-          quality: "highestaudio",
-          highWaterMark: 1 << 25,
+          quality: "lowestaudio",
+          highWaterMark: 1 << 50,
           opusEncoded: true,
         });
 
