738a3cedd277732d4e1850acfbfb574ccd10bbbd
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-07 21:38:48 +0700

    Update stats.ts

--- commands/stats.ts
+++ commands/stats.ts
@@ -4,6 +4,10 @@ const { platform, arch, cpus } = require("os")
 exports.run = async(client, message, args) => {
   const model = cpus()[0].model + " " + arch()
   const tanggalBuat = client.user.createdAt
+
+  const filter = (reaction, user) => {
+    return [`❎`].includes(reaction.emoji.name) && user.id === message.author.id;
+  };
   
   const embed = new MessageEmbed()
   .setColor("RANDOM")
@@ -17,5 +21,14 @@ CPU: ${model}
 Platform: ${platform}
 Websocket: ${client.ws.ping} ms(miliseconds)`)
   
-  message.channel.send(embed)
+message.channel.send(embed).then(embedMessage => {
+  embedMessage.react(`❎`);
+  embedMessage.awaitReactions(filter, { max: 1}).then(collected =>{
+      const reaction = collected.first();
+
+      if (reaction.emoji.name === `❎`){
+          embedMessage.delete(embed);
+      }
+  });
+});
 }
