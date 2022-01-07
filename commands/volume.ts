de75ea4039d97468e015357c8b9ee03d62f51242
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-07 21:57:02 +0700

    Update volume.ts

--- commands/volume.ts
+++ commands/volume.ts
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
   if (!channel)
     return message.channel.send(
       "KAMU HARUS JOIN CHANNEL DULU NAK!"
-    );
+    ).then(message => message.delete({timeout: 10000}));
 
   let queue = message.client.queue.get(message.guild.id);
 
@@ -18,7 +18,7 @@ exports.run = async (client, message, args) => {
         )
         .setColor("BLUE")
         .setDescription("**Current volume is " + queue.volume + " **")
-    );
+    ).then(message => message.delete({timeout: 10000}));
 
   if (args[0] > 100)
     return message.channel.send(
@@ -29,7 +29,7 @@ exports.run = async (client, message, args) => {
         )
         .setColor("RED")
         .setDescription("**Volume cannot exceed 100 :x: **")
-    );
+    ).then(message => message.delete({timeout: 10000}));
 
   queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
   queue.volume = args[0];
@@ -41,5 +41,5 @@ exports.run = async (client, message, args) => {
       )
       .setColor("BLUE")
       .setDescription("**Volume set to " + args[0] + " :white_check_mark: **")
-  );
+  ).then(message => message.delete({timeout: 10000}));
 };
