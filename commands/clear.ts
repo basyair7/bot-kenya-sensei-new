4b37bf549f970ae1f608355ebf67378452ec7f70
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-09 03:30:22 +0700

    Update clear.ts

--- commands/clear.ts
+++ commands/clear.ts
@@ -7,6 +7,6 @@ module.exports = {
         if(parseInt(args[0]) > 99) return message.channel.send('The max amount of messages that I can delete is 99')
         await message.channel.bulkDelete(parseInt(args[0]) + 1)
             .catch(err => console.log(err))
-        message.channel.send('Deleted ' + args[0]  + " messages.")
+        message.channel.send('Deleted ' + args[0]  + " messages.").then(msg => msg.delete({timeout: 5000}));
     }
 }
