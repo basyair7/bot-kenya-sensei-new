b6fc9b6b352e1fad6cb6fa360d90891ea639f481
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-09 04:02:32 +0700

    Update clear.ts

--- commands/clear.ts
+++ commands/clear.ts
@@ -2,8 +2,8 @@ module.exports = {
     name : 'clear',
     aliases : ['purge'],
     run : async(client, message, args) => {
-        if(!args[0]) return message.channel.send('Please specify a number of messages to delete ranging from 1 - 99')
-        if(isNaN(args[0])) return message.channel.send('Numbers are only allowed')
+        if(!args[0]) return message.channel.send('Please specify a number of messages to delete ranging from 1 - 99').then(msg => msg.delete({timeout: 10000}));
+        if(isNaN(args[0])) return message.channel.send('Numbers are only allowed').then(msg => msg.delete({timeout: 10000}));
         if(parseInt(args[0]) > 99) return message.channel.send('The max amount of messages that I can delete is 99')
         await message.channel.bulkDelete(parseInt(args[0]) + 1)
             .catch(err => console.log(err))
