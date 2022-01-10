b700a7c9ac60891fbbb9ce7b249170e9c7723a25
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-10 13:53:14 +0700

    Update resume.ts

--- commands/resume.ts
+++ commands/resume.ts
@@ -2,25 +2,32 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.member.voice.channel;
-  if (!channel)
+  if (!channel){
+    message.delete({timeout: 1000});
     return message.channel.send(
       "KAMU HARUS JOIN CHANNEL DULU NAK!"
     ).then(message => message.delete({timeout: 10000}));
+  }
   let queue = message.client.queue.get(message.guild.id);
-  if (!queue)
+  if (!queue){
+    message.delete({timeout: 1000});
     return message.channel.send(
       new MessageEmbed()
         .setDescription(":x: There are no songs playing in this server")
         .setColor("RED")
     ).then(message => message.delete({timeout: 10000}));
-  if (queue.playing == true)
+  }
+  if (queue.playing == true){
+    message.delete({timeout: 1000});
     return message.channel.send(
       new MessageEmbed()
         .setDescription(":x: The song is already playing")
         .setColor("RED")
     ).then(message => message.delete({timeout: 10000}));
+  }
   queue.connection.dispatcher.resume();
   message.react("▶");
+  message.delete({timeout: 1000});
   queue.playing = true;
   return message.channel.send(
     new MessageEmbed()
