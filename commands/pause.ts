dcf698b736c3c8c9a01716053d20061e3fccab83
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-10 13:52:13 +0700

    Update pause.ts

--- commands/pause.ts
+++ commands/pause.ts
@@ -2,26 +2,33 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.member.voice.channel;
-  if (!channel)
+  if (!channel){
+    message.delete({timeout: 1000});
     return message.channel.send(
         "KAMU HARUS JOIN CHANNEL DULU NAK!"
     ).then(message => message.delete({timeout: 10000}));
+  }
   let queue = message.client.queue.get(message.guild.id);
-  if (!queue)
+  if (!queue){
+    message.delete({timeout: 1000});
     return message.channel.send(
       new MessageEmbed()
         .setDescription(":x: There are no songs playing in this server")
         .setColor("RED")
     ).then(message => message.delete({timeout: 10000}));
-  if (queue.playing == false)
+  }
+  if (queue.playing == false){
+    message.delete({timeout: 1000});
     return message.channel.send(
       new MessageEmbed()
         .setDescription(":x: The song is already paused")
         .setColor("RED")
     ).then(message => message.delete({timeout: 10000}));
+  }
   queue.connection.dispatcher.pause();
   message.react("⏸");
   queue.playing = false;
+  message.delete({timeout: 1000});
   return message.channel.send(
     new MessageEmbed()
     .setDescription("**Paused the music :white_check_mark: **")
