7588a23d238cb2820a9d9894eec1b2de90a42e2d
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-10 13:48:28 +0700

    Update stop.ts

--- commands/stop.ts
+++ commands/stop.ts
@@ -2,20 +2,24 @@ const { MessageEmbed } = require("discord.js");
 
 exports.run = async (client, message) => {
   const channel = message.member.voice.channel;
-  if (!channel)
+  if (!channel){
+    message.delete({timeout: 1000});
     return message.channel.send(
       "KAMU HARUS JOIN CHANNEL DULU NAK!"
     ).then(message => message.delete({timeout: 10000}));
+  }
   let queue = message.client.queue.get(message.guild.id);
-  if (!queue)
+  if (!queue){
+    message.delete({timeout: 1000});
     return message.channel.send(
       new MessageEmbed()
         .setDescription(":x: There are no songs playing in this server")
         .setColor("RED")
-    ).then(message => message.delete({timeout: 10000}));
+    ).then(message => message.delete({timeout: 10000}));}
 
   queue.connection.dispatcher.end();
   queue.queue = [];
+  message.delete({timeout: 1000});
   return message.channel.send(
     new MessageEmbed()
       .setDescription("**Stopped the music :white_check_mark: **")
