becc6bb397b1a19f7529c2f412d2470ae27940e4
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-11 11:35:23 +0700

    Update resume.ts

--- commands/resume.ts
+++ commands/resume.ts
@@ -3,14 +3,14 @@ const { MessageEmbed } = require("discord.js");
 exports.run = async (client, message) => {
   const channel = message.member.voice.channel;
   if (!channel){
-    message.delete({timeout: 1000});
+    
     return message.channel.send(
       "KAMU HARUS JOIN CHANNEL DULU NAK!"
     ).then(message => message.delete({timeout: 10000}));
   }
   let queue = message.client.queue.get(message.guild.id);
   if (!queue){
-    message.delete({timeout: 1000});
+    
     return message.channel.send(
       new MessageEmbed()
         .setDescription(":x: There are no songs playing in this server")
@@ -18,7 +18,7 @@ exports.run = async (client, message) => {
     ).then(message => message.delete({timeout: 10000}));
   }
   if (queue.playing == true){
-    message.delete({timeout: 1000});
+    
     return message.channel.send(
       new MessageEmbed()
         .setDescription(":x: The song is already playing")
@@ -27,7 +27,7 @@ exports.run = async (client, message) => {
   }
   queue.connection.dispatcher.resume();
   message.react("▶");
-  message.delete({timeout: 1000});
+  
   queue.playing = true;
   return message.channel.send(
     new MessageEmbed()
