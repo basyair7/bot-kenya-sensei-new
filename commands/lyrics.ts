c61beceef718b7472d26cc8557ac005bae45d563
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-16 00:09:15 +0700

    Update lyrics.ts

--- commands/lyrics.ts
+++ commands/lyrics.ts
@@ -39,7 +39,7 @@ exports.run = async (client, message, args) => {
                 const reaction = collected.first();
 
                 if (reaction.emoji.name === `❎`){
-                    embedMessage.delete(embed);
+                    embedMessage.delete({timeout: 5000});
                 }
             }).catch(collected => {console.log("error")});
         }).catch(console.error);
