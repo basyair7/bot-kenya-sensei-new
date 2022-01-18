a6199818f734ac264d1c182eea74883421e5e26d
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-18 21:44:51 +0700

    Update message.ts

--- events/message.ts
+++ events/message.ts
@@ -1,4 +1,18 @@
+const db = require('quick.db');
+
 module.exports = async (client, message) => {
+    if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
+        const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
+        await db.delete(`afk-${message.author.id}+${message.guild.id}`)
+        message.reply(`Your afk status have been removed (${info})`)
+    }
+    //checking for mentions
+    if(message.mentions.members.first()) {
+        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
+            message.channel.send(message.mentions.members.first().user.tag + ":" + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`))
+        }else return;
+    }else;
+
     // command not ks
     let notks = message.content.toLowerCase();
     if (notks === "help"){
