cb0fdb6bfa598fd9d48a6d6fe623356fa54c2c68
Fathul <61180722+basyair7@users.noreply.github.com>
2022-01-22 23:53:15 +0700

    Update help.ts

--- commands/help.ts
+++ commands/help.ts
@@ -17,25 +17,11 @@ exports.run = async (client, message) => {
   const cmd5 = '`ngakak`, `ohayou!`, `ohayou`, `oha`, `konnichiwa`, `konbanwa`, `anyeonghaseo`';
   const cmd6 = '`diam`, `diam!`, `diem lu`, `diem`, `diem lo`, `nugas`, `belajar`, `ada tugas`, `mau belajar`, `mau bljr`, `bljr`';
 
-  const commands = `connect\`\` - join the voice channel you are in
-   disconnect\`\` - leave the voice channel you are in
-   play <Song Name or url>\`\` - play songs from youtube
-   pause\`\` - pause currently playing songs in the server
-   resume\`\` - resume paused songs in the server
-   queue\`\` - shows the song queue of the server
-   skip\`\` - skips to next song in the queue
-   skipto <Target number>\`\` - Multiple skips until target
-   stop\`\` - stops the song and clears the queue
-   volume <volume count or none>\`\` - see or adjust volume of songs
-   np\`\` - see now playing song
-   lyrics\`\` - get lyrics of current song
-   shuffle\`\` - shuffle and randomize the queue
-   invite\`\` - get invite link for the bot
-   loop\`\` - enable / disable loop for the currently playing song
-   remove <Target number>\`\` - remove a song from the queue`;
+  const commands = `connect\`\`, disconnect\`\`, play <Song Name or url>\`\`, pause\`\`, resume\`\`, queue\`\`, skip\`\`, skipto <Target number>\`\`, stop\`\`,
+   volume <volume count or none>\`\`, np\`\`, lyrics\`\`, shuffle\`\`, invite\`\`,loop\`\`, remove <Target number>\`\`,`;
 
   const revised = commands
-    .split("\n")
+    .split(", ")
     .map((x) => "• " + "``" + client.config.prefix + x.trim())
     .join(", ");
   
