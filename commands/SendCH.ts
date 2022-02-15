e25311504eada2dfb7060482c2a7b9052f769336
Fathul <61180722+basyair7@users.noreply.github.com>
2022-02-15 12:13:06 +0700

    Delete SendCH.ts

--- commands/SendCH.ts
+++
@@ -1,8 +0,0 @@
-module.export = {
-  name: 'SendCH',
-  run: async(client, message, args) => {
-    var says = args.join(' ');
-    const channel01 = client.channels.cache.find(channel => channel.id "943006597641293854");
-    channel01.send(says);
-  }
-}
