a6b0fbb8ff759744df97a6e89bb0a2f8dbbb058a
Fathul <61180722+basyair7@users.noreply.github.com>
2022-02-15 11:53:02 +0700

    Update SendCH.ts

--- commands/SendCH.ts
+++ commands/SendCH.ts
@@ -1,1 +1,8 @@
-module.export = {}
+module.export = {
+  name: 'SendCH',
+  run: async(client, message, args) => {
+    var says = args.join(' ');
+    const channel01 = client.channels.cache.find(channel => channel.id "943006597641293854");
+    channel01.send(says);
+  }
+}
