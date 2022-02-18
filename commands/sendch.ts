bb5c014afd6502ea58cab3b3b10956f02216576b
Fathul <61180722+basyair7@users.noreply.github.com>
2022-02-18 16:51:11 +0700

    Update sendch.ts

--- commands/sendch.ts
+++ commands/sendch.ts
@@ -2,15 +2,15 @@ module.exports = {
     name: 'send ch',
     run: async(client, message, args) => {
         try{
-            let chselection = args.join(' ');
+        
             let selection = message.content.split(' | ');
-            if(chselection === "ch01"){
+            if(args[0] === "ch01"){
                const ch01 = client.channels.cache.find(channel => channel.id === "798157102249541684")
-               ch01.send(selection[1])
+               ch01.send(selection[0])
             }
-            else if(chselection === "ch02"){
+            else if(args[0] === "ch02"){
                const ch02 = client.channels.cache.find(channel => channel.id === "929327327219957821")
-               ch02.send(selection[1])
+               ch02.send(selection[0])
             }
         } catch(e){
             console.log(e)
