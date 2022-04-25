5408401c594acbfe5d77d0a7be0bc4e21cf23b03
Fathul <61180722+basyair7@users.noreply.github.com>
2022-04-26 03:51:53 +0700

    Update lofi.ts

--- commands/lofi.ts
+++ commands/lofi.ts
@@ -5,8 +5,17 @@ exports.run = async(client, message, args) => {
   try{
     let cmd = args[0].toLowerCase();
     const channel = message.member.voice.channel;
+
     if(cmd === "help"){
-      const list = `play\`\` => Play lofi hip hop radio 24/7,
+      const { MessageEmbed } = require("discord.js");
+const ytdl = require("ytdl-core");
+
+exports.run = async(client, message, args) => {
+  try{
+    let cmd = args[0].toLowerCase();
+    const channel = message.member.voice.channel;
+
+    const list = `play\`\` => Play lofi hip hop radio 24/7,
                 stop\`\` => Stop the song and leave the voice channel you are in `;
 
                 const filter = (reaction, user) => {
@@ -33,9 +42,8 @@ exports.run = async(client, message, args) => {
                            }
                        }).catch(collected => {console.log("Error")});
                 });
-    }
 
-    else if(cmd === "play"){
+    if(cmd === "play"){
       
       if (!channel)
         return message.channel.send(
@@ -54,7 +62,60 @@ exports.run = async(client, message, args) => {
 
       message.channel.send(
         new MessageEmbed()
-        .setTitle("Play Lofi Song 24/7")
+        .setTitle("Started Playing", "https://img.icons8.com/color/2x/cd--v3.gif")
+        .setDescription(`Playing Lofi Hip Hop Radio 24/7 for ${message.author}`)
+        .setColor("BLUE")
+      ).then(msg => msg.delete({timeout: 10000}));
+      
+    }
+
+    else if(cmd === "stop"){
+      if (!channel){
+        return message.channel.send(
+          "KAMU HARUS JOIN CHANNEL DULU NAK!"
+        ).then(message => message.delete({timeout: 10000}));
+      }
+      
+      await channel.leave();
+
+      return message.channel.send(
+        new MessageEmbed()
+          .setTitle("Stop Playing Lofi Song 24/7")
+          .setDescription("**Left the voice channel :white_check_mark: **")
+          .setColor("BLUE")
+      ).then(message => message.delete({timeout: 10000}));
+    }
+
+    else{
+      message.reply("Perintah tidak tersedia...").then(message => message.delete({timeout: 10000}));
+    }
+    
+  } catch(e){
+    console.log(e);
+  }
+}
+    }
+
+    if(cmd === "play"){
+      
+      if (!channel)
+        return message.channel.send(
+          "KAMU HARUS JOIN CHANNEL DULU NAK!"
+        ).then(message => message.delete({timeout: 10000}));
+
+      if (!channel.permissionsFor(message.client.user).has("CONNECT"))
+        return console.error("I don't have permission to join the voice channel");
+    
+      if (!channel.permissionsFor(message.client.user).has("SPEAK"))
+        return console.error("I don't have permission to speak in the voice channel");
+
+      const connect = await channel.join();
+      connect.play(ytdl("https://youtu.be/5qap5aO4i9A"));
+      connect.voice.setSelfDeaf(true);
+
+      message.channel.send(
+        new MessageEmbed()
+        .setTitle("Started Playing", "https://img.icons8.com/color/2x/cd--v3.gif")
         .setDescription(`Playing Lofi Hip Hop Radio 24/7 for ${message.author}`)
         .setColor("BLUE")
       ).then(msg => msg.delete({timeout: 10000}));
