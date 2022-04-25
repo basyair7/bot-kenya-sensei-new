89a82c7de0ce477dd1877f75d0857f05db7ab09e
Fathul <61180722+basyair7@users.noreply.github.com>
2022-04-26 03:42:52 +0700

    Create lofi.ts

---
+++ commands/lofi.ts
@@ -0,0 +1,87 @@
+const { MessageEmbed } = require("discord.js");
+const ytdl = require("ytdl-core");
+
+exports.run = async(client, message, args) => {
+  try{
+    let cmd = args[0].toLowerCase();
+    const channel = message.member.voice.channel;
+    if(cmd === "help"){
+      const list = `play\`\` => Play lofi hip hop radio 24/7,
+                stop\`\` => Stop the song and leave the voice channel you are in `;
+
+                const filter = (reaction, user) => {
+                      return [`❎`].includes(reaction.emoji.name) && user.id === message.author.id;
+                };
+
+                const revisedList = list
+                                .split('\n')
+                                .map((x) => "• " + "``" + client.config.prefix + "sendch " + x.trim())
+                                .join('\n');
+
+                const view_msg = new MessageEmbed()
+                        .setTitle("Play Lofi Song 24/7")
+                        .setDescription("Play Lofi Hip Hop Radio 24/7 https://youtu.be/5qap5aO4i9A")
+                        .addField("List Channel", revisedList)
+
+                return message.channel.send(view_msg).then(embedMessage => {
+                       embedMessage.react(`❎`);
+                       embedMessage.awaitReactions(filter, { max: 1 }).then(collected =>{
+                           const reaction = collected.first();
+
+                           if (reaction.emoji.name === `❎`){
+                               embedMessage.delete({timeout: 5000});
+                           }
+                       }).catch(collected => {console.log("Error")});
+                });
+    }
+
+    else if(cmd === "play"){
+      
+      if (!channel)
+        return message.channel.send(
+          "KAMU HARUS JOIN CHANNEL DULU NAK!"
+        ).then(message => message.delete({timeout: 10000}));
+
+      if (!channel.permissionsFor(message.client.user).has("CONNECT"))
+        return console.error("I don't have permission to join the voice channel");
+    
+      if (!channel.permissionsFor(message.client.user).has("SPEAK"))
+        return console.error("I don't have permission to speak in the voice channel");
+
+      const connect = await channel.join().then(connection => connection.voice.setSelfDeaf(true));
+
+      connect.play(ytdl("https://youtu.be/5qap5aO4i9A"));
+
+      message.channel.send(
+        new MessageEmbed()
+        .setTitle("Play Lofi Song 24/7")
+        .setDescription(`Playing Lofi Hip Hop Radio 24/7 for ${message.author}`)
+        .setColor("BLUE")
+      ).then(msg => msg.delete({timeout: 10000}));
+      
+    }
+
+    else if(cmd === "stop"){
+      if (!channel){
+        return message.channel.send(
+          "KAMU HARUS JOIN CHANNEL DULU NAK!"
+        ).then(message => message.delete({timeout: 10000}));
+      }
+      
+      await channel.leave();
+
+      return message.channel.send(
+        new MessageEmbed()
+          .setDescription("**Left the voice channel :white_check_mark: **")
+          .setColor("BLUE")
+      ).then(message => message.delete({timeout: 10000}));
+    }
+
+    else{
+      message.reply("Perintah tidak tersedia...").then(message => message.delete({timeout: 10000}));
+    }
+    
+  } catch(e){
+    console.log(e);
+  }
+}
