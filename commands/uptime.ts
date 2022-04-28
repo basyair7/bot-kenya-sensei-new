8a4216a65ed7bad7baafd50e8b73d095d79f58f6
Fathul <61180722+basyair7@users.noreply.github.com>
2022-04-29 05:42:01 +0700

    Update uptime.ts

--- commands/uptime.ts
+++ commands/uptime.ts
@@ -1,9 +1,11 @@
 const ms = require("pretty-ms")
 const { MessageEmbed } = require("discord.js")
+let DateTimeBot = require("../model/DateTimeBot.js");
 
 exports.run = async(client, message, args) => {
   const uptime = ms(client.uptime, {verbose:true})
   const embed = new MessageEmbed()
+  .addField("Date", DateTimeBot)
   .addField("Bot Uptime", uptime)
   .setColor("RANDOM")
   
