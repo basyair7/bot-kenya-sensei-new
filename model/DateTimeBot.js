9086f68a3ef1dbaebef383cb2bfe9b5b7dbfcf28
Fathul <61180722+basyair7@users.noreply.github.com>
2022-04-29 05:39:03 +0700

    Create DateTimeBot.js

---
+++ model/DateTimeBot.js
@@ -0,0 +1,19 @@
+const { DateTime } = require("luxon");
+
+
+var local = DateTime.local();
+var rezonedString = local.setZone("Asia/Jakarta");
+
+// get Date
+let year = rezonedString.c.year;
+let month = rezonedString.c.month;
+let day = rezonedString.c.day;
+
+// get Time
+let hour = rezonedString.c.hour;
+let minute = rezonedString.c.minute;
+let second = rezonedString.c.second;
+
+let datetime = year+"/"+month+"/"+day+" ("+hour+":"+minute+":"+second+")";
+
+module.exports = datetime;
