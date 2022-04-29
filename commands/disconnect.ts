2051bd56643816e30d8a5e82d771d9b227fe2dcd
Fathul <61180722+basyair7@users.noreply.github.com>
2022-04-30 00:19:46 +0700

    Update disconnect.ts

--- commands/disconnect.ts
+++ commands/disconnect.ts
@@ -1,10 +1,9 @@
 const ms = require("pretty-ms");
 const { MessageEmbed } = require("discord.js");
-var DateTimeBot = require("../model/DateTimeBot.js");
 
 exports.run = async (client, message) => {
   const channel = message.member.voice.channel;
-  client.user.setActivity(`Online ${DateTimeBot}`);
+  client.user.setActivity(`Online ${DateTimeBot()}`);
   /*
   setInterval(() => {
           const uptime = ms(client.uptime, {verbose:true});
@@ -26,3 +25,23 @@ exports.run = async (client, message) => {
       .setColor("BLUE")
   ).then(message => message.delete({timeout: 10000}));
 };
+
+var { DateTime } = require("luxon");
+
+function DateTimeBot(){
+  var local = DateTime.local();
+  var rezonedString = local.setZone("Asia/Jakarta");
+  
+  // get Date
+  let year = rezonedString.c.year;
+  let month = rezonedString.c.month;
+  let day = rezonedString.c.day;
+  
+  // get Time
+  let hour = rezonedString.c.hour;
+  let minute = rezonedString.c.minute;
+  let second = rezonedString.c.second;
+  
+  let datetime = year+"/"+month+"/"+day+" ("+hour+":"+minute+":"+second+")";
+  return datetime;
+}
