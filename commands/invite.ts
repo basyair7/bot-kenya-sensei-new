1d71801ab43a3ecdf46af36f464c11ebca32ca09
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 01:05:36 +0700

    Update invite.ts

--- commands/invite.ts
+++ commands/invite.ts
@@ -1,21 +1,27 @@
 const { MessageEmbed } = require("discord.js");
+const { addReport } = require("../model/dbReport");
 
 exports.run = async (client, message) => {
-  message.channel.send(
-    new MessageEmbed()
-      .setAuthor(
-        "Invite Me",
-        "https://img.icons8.com/ultraviolet/2x/email-open--v2.gif"
-      )
-      .setColor("BLUE")
-      .setTimestamp()
-      .setDescription(
-        "https://discord.com/oauth2/authorize?client_id=" +
-          client.user.id +
-          "&permissions=" +
-          "37080128" +
-          "&scope=" +
-          "bot"
-      )
-  );
+  try {
+    message.channel.send(
+      new MessageEmbed()
+        .setAuthor(
+          "Invite Me",
+          "https://img.icons8.com/ultraviolet/2x/email-open--v2.gif"
+        )
+        .setColor("BLUE")
+        .setTimestamp()
+        .setDescription(
+          "https://discord.com/oauth2/authorize?client_id=" +
+            client.user.id +
+            "&permissions=" +
+            "37080128" +
+            "&scope=" +
+            "bot"
+        )
+    );
+  } catch (error) {
+    console.log(error);
+    addReport(`Bot-Error`, `invite.ts Error: ${error}`);
+  }
 };
