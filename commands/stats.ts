2d0a5d94035c71e11a6efb734567e1564d3f69a2
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 01:45:39 +0700

    add mode dbReport

--- commands/stats.ts
+++ commands/stats.ts
@@ -1,34 +1,40 @@
-const { MessageEmbed } = require("discord.js")
-const { platform, arch, cpus } = require("os")
+const { MessageEmbed } = require("discord.js");
+const { platform, arch, cpus } = require("os");
+const {addReport} = require("../model/dbReport");
 
 exports.run = async(client, message, args) => {
-  const model = cpus()[0].model + " " + arch()
-  const tanggalBuat = client.user.createdAt
+  try {
+    const model = cpus()[0].model + " " + arch()
+    const tanggalBuat = client.user.createdAt
 
-  const filter = (reaction, user) => {
-    return [`❎`].includes(reaction.emoji.name) && user.id === message.author.id;
-  };
-  
-  const embed = new MessageEmbed()
-  .setColor("RANDOM")
-  .setTitle("Bot Statistics")
-  .setThumbnail(client.user.displayAvatarURL())
-  .addField("Bot", `
-Username: ${client.user.username}
-Tanggal Dibuat: ${tanggalBuat}`)
-  .addField("System", `
-CPU: ${model}
-Platform: ${platform}
-Websocket: ${client.ws.ping} ms(miliseconds)`)
-  
-message.channel.send(embed).then(embedMessage => {
-  embedMessage.react(`❎`);
-  embedMessage.awaitReactions(filter, { max: 1}).then(collected =>{
-      const reaction = collected.first();
+    const filter = (reaction, user) => {
+      return [`❎`].includes(reaction.emoji.name) && user.id === message.author.id;
+    };
+    
+    const embed = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle("Bot Statistics")
+    .setThumbnail(client.user.displayAvatarURL())
+    .addField("Bot", `
+      Username: ${client.user.username}
+      Tanggal Dibuat: ${tanggalBuat}`)
+        .addField("System", `
+      CPU: ${model}
+      Platform: ${platform}
+      Websocket: ${client.ws.ping} ms(miliseconds)`)
+        
+    message.channel.send(embed).then(embedMessage => {
+      embedMessage.react(`❎`);
+      embedMessage.awaitReactions(filter, { max: 1}).then(collected =>{
+          const reaction = collected.first();
 
-      if (reaction.emoji.name === `❎`){
-          embedMessage.delete(embed);
-      }
-  }).catch(collected => {console.log("Error")});
-});
+          if (reaction.emoji.name === `❎`){
+              embedMessage.delete(embed);
+          }
+      }).catch(collected => {console.log("Error")});
+  });
+  } catch (e) {
+    console.log(e);
+    addReport(`Bot-Error`, `stats.ts Error: ${e}`);
+  }
 }
