601927f392fce03f308f0e9a68a2e2e8c3c6133d
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 01:00:16 +0700

    Update disconnect.ts

--- commands/disconnect.ts
+++ commands/disconnect.ts
@@ -1,29 +1,35 @@
 const ms = require("pretty-ms");
 const { MessageEmbed } = require("discord.js");
+const {addReport} = require('../model/dbReport')
 
 exports.run = async (client, message) => {
-  const channel = message.member.voice.channel;
-  client.user.setActivity(`Online ${DateTimeBot()}`);
-  /*
-  setInterval(() => {
-          const uptime = ms(client.uptime, {verbose:true});
-          client.user.setActivity(`Online at ${uptime}`);
-  }, 3000);
-  */
-  
-  if (!channel){
+  try {
+    const channel = message.member.voice.channel;
+    client.user.setActivity(`Online ${DateTimeBot()}`);
+    /*
+    setInterval(() => {
+            const uptime = ms(client.uptime, {verbose:true});
+            client.user.setActivity(`Online at ${uptime}`);
+    }, 3000);
+    */
+    
+    if (!channel){
+      return message.channel.send(
+        "KAMU HARUS JOIN CHANNEL DULU NAK!"
+      ).then(message => message.delete({timeout: 10000}));
+    }
+
+    await channel.leave();
+
     return message.channel.send(
-      "KAMU HARUS JOIN CHANNEL DULU NAK!"
+      new MessageEmbed()
+        .setDescription("**Left the voice channel :white_check_mark: **")
+        .setColor("BLUE")
     ).then(message => message.delete({timeout: 10000}));
+  } catch (err) {
+    console.log(err);
+    addReport(`Bot-Error`, `disconnect.ts Error: ${err}`);
   }
-
-  await channel.leave();
-
-  return message.channel.send(
-    new MessageEmbed()
-      .setDescription("**Left the voice channel :white_check_mark: **")
-      .setColor("BLUE")
-  ).then(message => message.delete({timeout: 10000}));
 };
 
 var { DateTime } = require("luxon");
