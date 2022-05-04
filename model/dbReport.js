c9f9115a465d7ce1927cb0b14b56ecab7aee0bd1
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-04 19:23:18 +0700

    Create dbReport.js

---
+++ model/dbReport.js
@@ -0,0 +1,37 @@
+const firebase = require('../db/firebaseConfig');
+
+const readReason = (id, callback) => {
+    firebase.ref('report').child(id).once(
+        'value',
+        (snapshot) => {
+            callback(snapshot.val());
+        },
+        (err) => {
+            console.log('Read failed' + err.name);
+        }
+
+    )
+}
+
+const readId = (subject, callback) => {
+    firebase.ref('/report/'+subject+'/').once(
+        'value',
+        (snapshot) => {
+            callback(snapshot.val());
+        }, (err) => {
+            console.log('Read failed' + err.name);
+        }
+    )
+}
+
+const addData = (subject, content, callback) => {
+    firebase.ref('/report/'+subject+'/message/').set(content);
+    firebase.ref('/report/'+subject+'/subject/').set(subject);
+}
+
+const removeData = (value) => {
+    firebase.ref('/report/'+value+'/').remove();
+}
+
+module.exports = { readId, readReason, addData, removeData };
+
