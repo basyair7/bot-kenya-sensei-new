4ae8ec5fe2318fadf3d98af1073ae135e6c98bba
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 00:50:17 +0700

    Update message.ts

--- events/message.ts
+++ events/message.ts
@@ -1,63 +1,19 @@
-//const db = require('quick.db');
 const { MessageEmbed } = require("discord.js");
+const { addReport } = require("../model/dbReport");
 const firebase = require('../db/firebaseConfig');
 const { removeData, readReason, readId } = require('../model/dbModel');
-const { addReport } = require("../model/dbReport");
+//const db = require('quick.db');
 
 module.exports = async (client, message) => {
     try{
-    if(message.mentions.members.first()) {
-        let id = `afk-${message.mentions.members.first().id}and${message.guild.id}`;
-        firebase.ref('/afk/'+id+'/id/').once('value', function (snapshot) {
-            var data = snapshot.val()
-            if(data !== null) {
-                if(data === id){
-                    firebase.ref('/afk/'+id+'/reason/').once('value', function(SnapShot) {
-                        var msgReason = SnapShot.val()
-                        if(msgReason !== null) {
-                            const image = message.mentions.users.first()
-                            let description = message.mentions.members.first().user.tag + " : " + msgReason
-                            let msg = new MessageEmbed()
-                                .setTitle("Info Anggota AFK")
-                                .setThumbnail(image.displayAvatarURL())
-                                .setDescription(description)
-                            message.reply(msg).then(m => m.delete({timeout: 12000}))
-                        } else;
-                    })
-                } else return;
-            } else;
-        })
-    } else;
-
-
-    /*
-    if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
-        const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
-        await db.delete(`afk-${message.author.id}+${message.guild.id}`)
-        message.reply(`Your afk status have been removed (${info})`)
-    }
-    //checking for mentions
-    if(message.mentions.members.first()) {
-        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
-            const image = message.mentions.users.first()
-            let description = message.mentions.members.first().user.tag + " : " + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`)
-            let msg = new MessageEmbed()
-                .setTitle("Info Anggota AFK")
-                .setThumbnail(image.displayAvatarURL())
-                .setDescription(description)
-            message.reply(msg)
-        }else return;
-    }else;
-    */
-    } catch(e){
-        addReport(`Bot-Error`, `message.ts Error: ${e}`);
-        console.log(e);
-    }
+    // Program Afk Client
+    programAfk(client, message);    
 
     // command not ks
     let notks = message.content.toLowerCase();
+
     if (notks === "help"){
-        message.channel.send("ks?help").then(msg => msg.delete({timeout: 3000}));
+        message.channel.send("ks.help").then(msg => msg.delete({timeout: 3000}));
     }
 
     else if (notks === "indonesia") {
@@ -178,4 +134,83 @@ module.exports = async (client, message) => {
 
     cmd.run(client, message, args);
 
+} catch(e){
+    addReport(`Bot-Error`, `message.ts Error: ${e}`);
+    console.log(e);
+}
+
 };
+
+function programAfk(client, message){
+    const argAfk = message.content
+        .slice(client.config.prefix.length)
+        .trim()
+        .split(/ +/g);
+    const msgAfk = argAfk.shift().toLowerCase();
+
+    if (msgAfk !== "afk"){
+        firebase.ref(`/afk/afk-${message.author.id}and${message.guild.id}`).once('value').then((snapshot) => {
+            var idMember = snapshot.val();
+            if (idMember !== null)
+            {
+                if (idMember.id === `afk-${message.author.id}and${message.guild.id}`) {
+                    firebase.ref(`/afk/${`afk-${message.author.id}and${message.guild.id}`}/reason/`).once('value').then(async (snapshot) => {
+                        var reason = snapshot.val();
+                        if (reason !== null)
+                        {
+                            message.reply(`Selamat Datang nak... Kamu telah kembali dari (${reason})`).then(msg => msg.delete({ timeout: 10000 }))
+                            removeData(`afk-${message.author.id}and${message.guild.id}`)
+                        } else;
+                    })
+                } else return;
+            } else;
+        })
+    }
+    
+    if(message.mentions.members.first()) {
+        let id = `afk-${message.mentions.members.first().id}and${message.guild.id}`;
+        firebase.ref(`/afk/${id}/id/`).once('value', function (snapshot) {
+            var data = snapshot.val()
+            if(data !== null)
+            {
+                if(data === id)
+                {
+                    firebase.ref(`/afk/${id}/reason/`).once('value', function(SnapShot) {
+                        var msgReason = SnapShot.val()
+                        if(msgReason !== null)
+                        {
+                            const image = message.mentions.users.first()
+                            let description = message.mentions.members.first().user.tag + " : " + msgReason
+                            let msg = new MessageEmbed()
+                                .setTitle("Info Anggota AFK")
+                                .setThumbnail(image.displayAvatarURL())
+                                .setDescription(description)
+                            message.reply(msg).then(m => m.delete({timeout: 12000}))
+                        
+                        } else;
+                    })
+                } else return;
+            } else;
+        })
+    } else;
+
+    /*
+    if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
+        const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
+        await db.delete(`afk-${message.author.id}+${message.guild.id}`)
+        message.reply(`Your afk status have been removed (${info})`)
+    }
+    //checking for mentions
+    if(message.mentions.members.first()) {
+        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
+            const image = message.mentions.users.first()
+            let description = message.mentions.members.first().user.tag + " : " + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`)
+            let msg = new MessageEmbed()
+                .setTitle("Info Anggota AFK")
+                .setThumbnail(image.displayAvatarURL())
+                .setDescription(description)
+            message.reply(msg)
+        }else return;
+    }else;
+    */
+}
