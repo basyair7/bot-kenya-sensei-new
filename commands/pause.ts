bb865318e1d35553f79e6118c9a1081cdf7eb370
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 01:18:11 +0700

    Update pause.ts

--- commands/pause.ts
+++ commands/pause.ts
@@ -1,37 +1,43 @@
 const { MessageEmbed } = require("discord.js");
+const {addReport} = require("../model/dbReport");
 
 exports.run = async (client, message) => {
-  const channel = message.member.voice.channel;
-  if (!channel){
-    
-    return message.channel.send(
-        "KAMU HARUS JOIN CHANNEL DULU NAK!"
-    ).then(message => message.delete({timeout: 10000}));
-  }
-  let queue = message.client.queue.get(message.guild.id);
-  if (!queue){
+  try {
+    const channel = message.member.voice.channel;
+    if (!channel){
+      
+      return message.channel.send(
+          "KAMU HARUS JOIN CHANNEL DULU NAK!"
+      ).then(message => message.delete({timeout: 10000}));
+    }
+    let queue = message.client.queue.get(message.guild.id);
+    if (!queue){
+      
+      return message.channel.send(
+        new MessageEmbed()
+          .setDescription(":x: There are no songs playing in this server")
+          .setColor("RED")
+      ).then(message => message.delete({timeout: 10000}));
+    }
+    if (queue.playing == false){
+      
+      return message.channel.send(
+        new MessageEmbed()
+          .setDescription(":x: The song is already paused")
+          .setColor("RED")
+      ).then(message => message.delete({timeout: 10000}));
+    }
+    queue.connection.dispatcher.pause();
+    message.react("⏸");
+    queue.playing = false;
     
     return message.channel.send(
       new MessageEmbed()
-        .setDescription(":x: There are no songs playing in this server")
-        .setColor("RED")
+      .setDescription("**Paused the music :white_check_mark: **")
+      .setColor("BLUE")
     ).then(message => message.delete({timeout: 10000}));
+  } catch (e) {
+    console.log(e);
+    addReport(`Bot-Error`, `pause.ts Error: ${e}`);
   }
-  if (queue.playing == false){
-    
-    return message.channel.send(
-      new MessageEmbed()
-        .setDescription(":x: The song is already paused")
-        .setColor("RED")
-    ).then(message => message.delete({timeout: 10000}));
-  }
-  queue.connection.dispatcher.pause();
-  message.react("⏸");
-  queue.playing = false;
-  
-  return message.channel.send(
-    new MessageEmbed()
-    .setDescription("**Paused the music :white_check_mark: **")
-    .setColor("BLUE")
-  ).then(message => message.delete({timeout: 10000}));
 };
