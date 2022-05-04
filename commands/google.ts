cc9f23bfe11f5fc218d11e377bf7af7fe71fccae
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-04 18:30:43 +0700

    Update google.ts

--- commands/google.ts
+++ commands/google.ts
@@ -12,7 +12,7 @@ exports.run = async(client, message, args) => {
             key: "AIzaSyBzYYTqN_X90RKqQfJtXbr43m8sVn3UcvM"
         });
 
-        if (!result.body.items) return message.reply("Tidak Ditemukan. ");
+        if (!result.body.items) return message.reply(`Yang kamu cari "${query}" Tidak Ditemukan Nak.`).then(msg => msg.delete({timeout: 10000}));
         if (result.status >= 400) return message.channel.send("Error. ").then(console.log(result.message));
 
         let res = result.body.items[0];
@@ -22,7 +22,21 @@ exports.run = async(client, message, args) => {
         .setDescription(res.snippet)
         .setURL(res.link)
         .setImage(res.pagemap.cse_image[0].src || res.pagemap.cse_thumbnail[0].src);
-        return message.reply(embed);
+
+        const filter = (reaction, user) => {
+            return [`❎`].includes(reaction.emoji.name) && user.id === msg.author.id;
+        }
+
+        return message.reply(embed).then(embedMessage => {
+            embedMessage.react(`❎`);
+            embedMessage.awaitReactions(filter, { max: 1 }).then(collected => {
+                const reaction = collected.first();
+
+                if (reaction.emoji.name === `❎`){
+                    embedMessage.delete({timeout: 5000});
+                }
+            }).catch(collected => {console.log("Error.")});
+        });
     } catch (error) {
         console.log(error);
     }
