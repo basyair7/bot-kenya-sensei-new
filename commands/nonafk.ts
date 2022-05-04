e21ec5cc994a096c98ae2b5e64655d557e74c56b
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-04 22:13:41 +0700

    Update nonafk.ts

--- commands/nonafk.ts
+++ commands/nonafk.ts
@@ -7,18 +7,21 @@ module.exports = {
         let idMember = `afk-${message.author.id}and${message.guild.id}`;
         firebase.ref('/afk/'+idMember+'/id/').once('value', function (snapshot) {
             const id = snapshot.val();
-            if (id !== null) {
-                if (idMember === id){
+            if (id !== null) 
+            {
+                if (idMember === id)
+                {
                     firebase.ref('/afk/'+idMember+'/reason/').once('value', 
                     (SnapShot) => {
                         var reason = SnapShot.val()
-                            if (reason !== null) {
+                            if (reason !== null) 
+                            {
                                 message.reply(`Selamat Datang nak... Kamu telah kembali dari (${reason})`).then(msg => msg.delete({ timeout: 10000 }))
                                 removeData(idMember)
                             } else;
                     })
-                }
-            }
+                } else return;
+            } else;
         })
     }
 }
