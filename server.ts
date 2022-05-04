dadef7d55993d147d5e596217ba1c3ebed4ab57a
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-04 19:52:23 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 const { Client, Collection, Intents } = require("discord.js");
+const { addReport } = require("./model/dbReport");
 const fs = require("fs");
 require("dotenv").config();
 
@@ -6,74 +7,79 @@ run(process.env.token, process.env.prefix);
 require("http").createServer((_,res) => res.end("Bot is online")).listen(8080);
 
 function run(token, prefix){
-  const client = new Client({ intent: [ Intents.ALL ]});
-  const config = {
-    token: token,
-    prefix: prefix
-  };
-
-  client.config = config;
-  client.queue = new Map();
-
-  // bot ready
-  client.on("ready", async () => {
-    console.log(`${client.user.tag} sudah online!`);
-    client.user.setUsername(`Kenya-sensei 「 ${config.prefix} 」`);
-    client.user.setActivity(`Online ${DateTimeBot()}`);
-
-    // Send Infomation Bot Online in Channel
-    const ch1 = "798163730982502400";
-    const ch2 = "929327327219957821";
-    let ch;
-    ch = client.channels.cache.find(channel => channel.id === ch1);
-    ch.send(`Sensei-bot Kembali (${DateTimeBot()})`);
-    ch = client.channels.cache.find(channel => channel.id === ch2);
-    ch.send(`Sensei-bot Kembali (${DateTimeBot()})`);
+  try {
+    const client = new Client({ intent: [ Intents.ALL ]});
+    const config = {
+      token: token,
+      prefix: prefix
+    };
+  
+    client.config = config;
+    client.queue = new Map();
+  
+    // bot ready
+    client.on("ready", async () => {
+      console.log(`${client.user.tag} sudah online!`);
+      client.user.setUsername(`Kenya-sensei 「 ${config.prefix} 」`);
+      client.user.setActivity(`Online ${DateTimeBot()}`);
+      addData(`History-Bot-re-login`, `Datetime : ${DateTimeBot()}`);
+  
+      // Send Infomation Bot Online in Channel
+      const ch1 = "798163730982502400";
+      const ch2 = "929327327219957821";
+      let ch;
+      ch = client.channels.cache.find(channel => channel.id === ch1);
+      ch.send(`Sensei-bot Kembali (${DateTimeBot()})`).then(msg => msg.delete({timeout: 10000}));
+      ch = client.channels.cache.find(channel => channel.id === ch2);
+      ch.send(`Sensei-bot Kembali (${DateTimeBot()})`).then(msg => msg.delete({timeout: 10000}));
+      
+    });
+  
+  
+    fs.readdir("./events/", (err, files) => {
+      if (err) return console.error(err);
+      files.forEach((file) => {
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        client.on(eventName, event.bind(null, client));
+      });
+    });
+  
+    client.commands = new Collection();
+  
+    fs.readdir("./commands/", (err, files) => {
+      if (err) return console.error(err);
+      files.forEach((file) => {
+        if (!file.endsWith(".ts")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        console.log(`[Command Manager]: Loading Command ${commandName}`);
+        client.commands.set(commandName, props);
+      });
+    });
+  
+    client.login(config.token);
+  } catch (error) {
+    addReport(`Bot-Error`, `Server.ts Error: ${error}`)
+  }
+} 
+  
+  var { DateTime } = require("luxon");
+  
+  function DateTimeBot(){
+    var local = DateTime.local();
+    var rezonedString = local.setZone("Asia/Jakarta");
     
-  });
-
-
-  fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach((file) => {
-      const event = require(`./events/${file}`);
-      let eventName = file.split(".")[0];
-      client.on(eventName, event.bind(null, client));
-    });
-  });
-
-  client.commands = new Collection();
-
-  fs.readdir("./commands/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach((file) => {
-      if (!file.endsWith(".ts")) return;
-      let props = require(`./commands/${file}`);
-      let commandName = file.split(".")[0];
-      console.log(`[Command Manager]: Loading Command ${commandName}`);
-      client.commands.set(commandName, props);
-    });
-  });
-
-  client.login(config.token);
-}
-
-var { DateTime } = require("luxon");
-
-function DateTimeBot(){
-  var local = DateTime.local();
-  var rezonedString = local.setZone("Asia/Jakarta");
-  
-  // get Date
-  let year = rezonedString.c.year;
-  let month = rezonedString.c.month;
-  let day = rezonedString.c.day;
-  
-  // get Time
-  let hour = rezonedString.c.hour;
-  let minute = rezonedString.c.minute;
-  let second = rezonedString.c.second;
-  
-  let datetime = year+"/"+month+"/"+day+" ("+hour+":"+minute+":"+second+")";
-  return datetime;
-}
+    // get Date
+    let year = rezonedString.c.year;
+    let month = rezonedString.c.month;
+    let day = rezonedString.c.day;
+    
+    // get Time
+    let hour = rezonedString.c.hour;
+    let minute = rezonedString.c.minute;
+    let second = rezonedString.c.second;
+    
+    let datetime = year+"/"+month+"/"+day+" ("+hour+":"+minute+":"+second+")";
+    return datetime;
+  }
