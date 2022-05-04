1aca3f295d669675af5befa94aa90c4a0e53deb1
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 01:20:26 +0700

    add dbReport

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,6 +1,12 @@
+const {addReport} = require("../model/dbReport");
 exports.run = async(client, message, args) => {
+  try {
     var ping = Date.now() - message.createdTimestamp;
     message.reply(`:ping_pong: Pong! ${ping}ms`);
+  } catch (e) {
+    console.log(e);
+    addReport(`Bot-Error`, `ping.ts Error: ${e}`);
+  }
 }
 
 /*
