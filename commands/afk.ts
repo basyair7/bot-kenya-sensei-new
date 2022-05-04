a42b6954091c4f850bda8203dad4869fc9f2b71a
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-04 20:35:10 +0700

    Update afk.ts

--- commands/afk.ts
+++ commands/afk.ts
@@ -1,18 +1,24 @@
 //requiring the package
-const db = require('quick.db')
-const { MessageEmbed } = require('discord.js')
-const { addData } = require('../model/dbModel')
+//const db = require('quick.db');
+const { MessageEmbed } = require('discord.js');
+const { addData } = require('../model/dbModel');
+const { addReport } = require("../model/dbReport");
 
 module.exports = {
     name: 'afk',
     run: async(client, message, args) => {
-        const content = args.join(' ');
-        const embed = new MessageEmbed()
-        .setDescription(`You have been set to afk\n**Reason :** ${content}`)
-        .setColor("RANDOM")
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
-        message.channel.send(embed).then(msg => msg.delete({timeout: 10000}))
-        return addData(`afk-${message.author.id}and${message.guild.id}`, content)
+        try {
+            const content = args.join(' ');
+            const embed = new MessageEmbed()
+            .setDescription(`You have been set to afk\n**Reason :** ${content}`)
+            .setColor("RANDOM")
+            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+            message.channel.send(embed).then(msg => msg.delete({timeout: 10000}))
+            return addData(`afk-${message.author.id}and${message.guild.id}`, content)
+        } catch (error) {
+            addReport(`Bot-Error`, `afk.ts Error: ${error}`);
+            console.log(error);
+        }
     }
 }
 
