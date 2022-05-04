7e97392169489d26bcb34530f539ae69d0f30705
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 01:38:17 +0700

    add mode dbReport

--- commands/skip.ts
+++ commands/skip.ts
@@ -1,32 +1,34 @@
 const { MessageEmbed } = require("discord.js");
+const {addReport} = require("../model/dbReport");
 
 exports.run = async (client, message) => {
   try{
-  const channel = message.member.voice.channel;
-  if (!channel){
-    message.delete({timeout: 1000});
-    return message.channel.send(
-      "KAMU HARUS JOIN CHANNEL DULU NAK!"
-    ).then(message => message.delete({timeout: 10000}));
-  }
-  
-  let queue = message.client.queue.get(message.guild.id);
-  if (!queue){
-    message.delete({timeout: 1000});
+    const channel = message.member.voice.channel;
+    if (!channel){
+      message.delete({timeout: 1000});
+      return message.channel.send(
+        "KAMU HARUS JOIN CHANNEL DULU NAK!"
+      ).then(message => message.delete({timeout: 10000}));
+    }
+    
+    let queue = message.client.queue.get(message.guild.id);
+    if (!queue){
+      message.delete({timeout: 1000});
+      return message.channel.send(
+        new MessageEmbed()
+          .setDescription(":x: There are no songs playing in this server")
+          .setColor("RED")
+      ).then(message => message.delete({timeout: 10000}));
+    }
+    
+    queue.connection.dispatcher.end('skipped');
     return message.channel.send(
       new MessageEmbed()
-        .setDescription(":x: There are no songs playing in this server")
-        .setColor("RED")
+        .setDescription("**Skipped the music :white_check_mark: **")
+        .setColor("BLUE")
     ).then(message => message.delete({timeout: 10000}));
+  } catch(e){
+      console.log(e);
+      addReport(`Bot-Error`, `skip.ts Error: ${e}`);
   }
-  
-  queue.connection.dispatcher.end('skipped');
-  return message.channel.send(
-    new MessageEmbed()
-      .setDescription("**Skipped the music :white_check_mark: **")
-      .setColor("BLUE")
-  ).then(message => message.delete({timeout: 10000}));
- } catch(e){
-     console.log(e);
- }
 };
