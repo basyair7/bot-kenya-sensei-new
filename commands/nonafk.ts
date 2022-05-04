3eb311887c5cd2fe2f8d93ffd4a13fc46bf3b658
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-04 22:16:12 +0700

    Update nonafk.ts

--- commands/nonafk.ts
+++ commands/nonafk.ts
@@ -1,27 +1,33 @@
 const firebase = require('../db/firebaseConfig');
 const { removeData } = require('../model/dbModel');
+const { addReport } = require('../model/dbReport');
 
 module.exports = {
     name: 'nonafk',
     run: async(client, message, args) => {
-        let idMember = `afk-${message.author.id}and${message.guild.id}`;
-        firebase.ref('/afk/'+idMember+'/id/').once('value', function (snapshot) {
-            const id = snapshot.val();
-            if (id !== null) 
-            {
-                if (idMember === id)
+        try{
+            let idMember = `afk-${message.author.id}and${message.guild.id}`;
+            firebase.ref('/afk/'+idMember+'/id/').once('value').then(async function (snapshot) {
+                const id = snapshot.val();
+                if (id !== null) 
                 {
-                    firebase.ref('/afk/'+idMember+'/reason/').once('value', 
-                    (SnapShot) => {
-                        var reason = SnapShot.val()
-                            if (reason !== null) 
-                            {
-                                message.reply(`Selamat Datang nak... Kamu telah kembali dari (${reason})`).then(msg => msg.delete({ timeout: 10000 }))
-                                removeData(idMember)
-                            } else;
-                    })
-                } else return;
-            } else;
-        })
+                    if (idMember === id)
+                    {
+                        firebase.ref('/afk/'+idMember+'/reason/').once('value', 
+                        (SnapShot) => {
+                            var reason = SnapShot.val()
+                                if (reason !== null) 
+                                {
+                                    message.reply(`Selamat Datang nak... Kamu telah kembali dari (${reason})`).then(msg => msg.delete({ timeout: 10000 }))
+                                    removeData(idMember)
+                                } else;
+                        })
+                    } else return;
+                } else;
+            })
+        } catch(e){
+            console.log(e);
+            addReport(`Bot-Error`, `nonafk.ts Error: ${e}`);
+        }
     }
 }
