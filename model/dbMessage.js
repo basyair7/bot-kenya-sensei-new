a2a915da03ae0dea454d16892fc0e706aa10e0bd
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 13:32:52 +0700

    Update dbMessage.js

--- model/dbMessage.js
+++ model/dbMessage.js
@@ -13,4 +13,24 @@ const getQNA = (callback) => {
     )
 };
 
-module.exports = { getQNA };
+const readSnipemsg = (callback) => {
+    firebase.ref('messages').once(
+        'value',
+        (snapshot) =>{
+            callback(snapshot.val());
+        },
+        (err) => {
+            console.log(`Read Failed: ${err.name}`);
+        }
+    )
+}
+
+const addSnipemsg = (datetime, content, author, authorid, channelid) => {
+    firebase.ref(`/messages/${datetime}/datetime/`).set(datetime);
+    firebase.ref(`/messages/${datetime}/author/`).set(author);
+    firebase.ref(`/messages/${datetime}/content/`).set(content);
+    firebase.ref(`/messages/${datetime}/authorid/`).set(authorid);
+    firebase.ref(`/messages/${datetime}/channelid/`).set(channelid);
+};
+
+module.exports = { getQNA, addSnipemsg, readSnipemsg };
