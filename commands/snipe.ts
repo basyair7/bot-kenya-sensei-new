cb51973cb9c257999d1078f1e5aa3d110577f22c
Fathul <61180722+basyair7@users.noreply.github.com>
2022-05-05 13:46:19 +0700

    Update snipe.ts

--- commands/snipe.ts
+++ commands/snipe.ts
@@ -1,19 +1,23 @@
 const {addReport} = require("../model/dbReport");
-const {readSnipemsg} = require("../model/dbMessage");
+const {readSnipemsg, removeSnipemsg} = require("../model/dbMessage");
 const { MessageEmbed } = require("discord.js");
 const firebase = require("../db/firebaseConfig");
 
 module.exports.run = async(client, message, args) => {
     try {
        var input = args[0];
+       let cmd = message.content.split('. ');
        var user = client.users.cache.get(input) || client.users.cache.find(x => x.username == input) || message.guild.members.cache.get(input)?.user || message.mentions.users.first() || message.author;
        
         readSnipemsg((data) => {
             Object.keys(data).map((key) =>{
-                // let SelectTime = message.content.split(': ');
                 let dbUser = data[key].author;
+                let dbUserid = data[key].authorid;
                 
                 if(user.tag === dbUser){
+                    if(cmd[1] === "delete"){
+                      removeSnipemsg(dbUserid);
+                    }
                     const msgembed = new MessageEmbed()
                     .setAuthor(dbUser, user.displayAvatarURL())
                     .setDescription(data[key].content)
